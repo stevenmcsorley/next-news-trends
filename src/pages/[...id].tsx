7b8a27bc52c1bd9e25c55c13769767e50e8f80c5
Steven McSorley <smcsorl@sky.com>
2020-11-08 12:39:11 +0000

    category pages populate part 1

---
+++ src/pages/[...id].tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container } from "@chakra-ui/core";
+import TopNews from "./../../components/topnews";
+
+import { fetchCategory } from "../../lib/category";
+
+const Article = ({ props, error }) => {
+  return (
+    <Container maxW="xl" centerContent>
+      <TopNews {...props.data} />
+    </Container>
+  );
+};
+
+Article.getInitialProps = async (ctx: { query: { id: any } }) => {
+  const id = {
+    section: ctx.query.id,
+  };
+
+  const data = await fetchCategory(id);
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
+
+export default Article;
